e28209a42649518c05fc245b32bbea3c4a5e478c
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2019-05-29 16:05:57 +0530

    Added more comments
    Change-Id: I5b17a67fa87b95d666352289692b2b179cb427d9

--- ECommerce/Models/ErrorViewModel.cs
+++ ECommerce/Models/ErrorViewModel.cs
@@ -2,8 +2,12 @@ using System;
 
 namespace ECommerce.Models
 {
+    /// <summary>
+    /// Error View Model
+    /// </summary>
     public class ErrorViewModel
     {
+
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
